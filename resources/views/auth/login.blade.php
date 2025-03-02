dd2c5ea7a5c8a45df5b7fac7778c55c7d983a9b5
zaahriinaann <znurpuriani@gmail.com>
2025-03-02 20:13:38 +0700

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <base href="">
+    <title>PPBI | Aplikasi Penilaian Estetika Bonsai @yield('title')</title>
+    <meta name="description"
+        content="Ceres admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets." />
+    <meta name="keywords"
+        content="Ceres theme, bootstrap, bootstrap 5, admin themes, free admin themes, bootstrap admin, bootstrap dashboard" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Ceres HTML Free - Bootstrap 5 HTML Multipurpose Admin Dashboard Theme" />
+    <meta property="og:url" content="https://keenthemes.com/products/ceres-html-pro" />
+    <meta property="og:site_name" content="Keenthemes | Ceres HTML Free" />
+    <link rel="canonical" href="Https://preview.keenthemes.com/ceres-html-free" />
+    {{-- <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}" /> --}}
+    {{-- https://ppbindonesia.com/wp-content/uploads/2024/05/cropped-Logo-PPBI-300x155.png --}}
+    <link rel="shortcut icon"
+        href="https://ppbindonesia.com/wp-content/uploads/2024/05/cropped-Logo-PPBI-300x155.png" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <link href="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body id="kt_body" style="background-image: url(assets/media/patterns/header-bg.png)"
+    class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled">
+    <div class="d-flex flex-column flex-root">
+        <div class="page d-flex flex-row flex-column-fluid">
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+                <div id="kt_content_container" class="d-flex flex-column-fluid align-items-start container-xxl">
+                    <div class="card m-auto">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="card-body">
+                                <h2 class="text-center mb-5 fs-1">Login</h2>
+                                <div class="text-center mb-5">
+                                    <img src="https://ppbindonesia.com/wp-content/uploads/2024/05/cropped-Logo-PPBI-300x155.png"
+                                        alt="" class="w-50">
+                                </div>
+                                <div class="form-group mb-2">
+                                    <label for="username">Username</label>
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus
+                                        placeholder="email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group mb-2">
+                                    <label for="password">Password</label>
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="current-password" placeholder="********">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="d-flex justify-content-end">
+                                    <button class="btn btn-primary btn-sm">Login</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                @include('layouts.footer')
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('template/src/plugins/global/plugins.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+    <script src="{{ asset('template/src/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <base href="">
+    <title>PPBI | Aplikasi Penilaian Estetika Bonsai @yield('title')</title>
+    <meta name="description"
+        content="Ceres admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets." />
+    <meta name="keywords"
+        content="Ceres theme, bootstrap, bootstrap 5, admin themes, free admin themes, bootstrap admin, bootstrap dashboard" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Ceres HTML Free - Bootstrap 5 HTML Multipurpose Admin Dashboard Theme" />
+    <meta property="og:url" content="https://keenthemes.com/products/ceres-html-pro" />
+    <meta property="og:site_name" content="Keenthemes | Ceres HTML Free" />
+    <link rel="canonical" href="Https://preview.keenthemes.com/ceres-html-free" />
+    {{-- <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}" /> --}}
+    {{-- https://ppbindonesia.com/wp-content/uploads/2024/05/cropped-Logo-PPBI-300x155.png --}}
+    <link rel="shortcut icon"
+        href="https://ppbindonesia.com/wp-content/uploads/2024/05/cropped-Logo-PPBI-300x155.png" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <link href="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body id="kt_body" style="background-image: url(assets/media/patterns/header-bg.png)"
+    class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled">
+    <div class="d-flex flex-column flex-root">
+        <div class="page d-flex flex-row flex-column-fluid">
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+                @include('layouts.header')
+                @include('layouts.toolbar')
+                <div id="kt_content_container" class="d-flex flex-column-fluid align-items-start container-xxl">
+                    @yield('content')
+                </div>
+                @include('layouts.footer')
+            </div>
+        </div>
+    </div>
+
+    @inlcude('layouts.etc')
+    <script src="{{ asset('template/src/plugins/global/plugins.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+    <script src="{{ asset('template/src/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
+</body>
+
+</html>
